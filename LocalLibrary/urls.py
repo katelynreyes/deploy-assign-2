211bb20d19421b7f90a2c63116f2cc0dcb31efb7
katelynreyes <katelynreyes@unomaha.edu>
2024-03-03 17:57:14 -0600

    for deployment

---
+++ LocalLibrary/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path, include, include, re_path
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('register.urls')),
+]+ static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+settings.MEDIA_ROOT}), #serve media files when deployed
+re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+settings.STATIC_ROOT}), #serve static files when deployed
+
+
+
